9b66f7966ccf1f03431bbb272ad26164868c7244
Niraj <nirajhirachan147@hotmail.com>
2018-06-30 13:06:59 +0545

    skeleton created

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// initialize our express app
+const app = express();
+
+
+
+
+
+
+
+let port = 1234;
+app.listen(port, () => {
+    console.log('Server is up and running on port numner ' + port);
+});
